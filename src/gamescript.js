5d756d05f8b35ed333b5913d857224c3315ecd8d
KaJIe4 <KaJIe4@gmail.com>
2018-05-12 13:19:23 +0500

    All sit

---
+++ src/gamescript.js
@@ -0,0 +1,142 @@
+var background;
+var gameLayer;
+var scrollSpeed = 1;
+var ship;
+var gameGravity = -0.05;
+var gameThrust = 0.1;
+var gameScene = cc.Scene.extend({
+onEnter:function () {
+this._super();
+gameLayer = new game();
+gameLayer.init();
+cc.audioEngine.playMusic("assets/1.mp3", true);
+this.addChild(gameLayer);
+}
+});
+var game = cc.Layer.extend({
+init:function () {
+this._super();
+
+
+cc.eventManager.addListener({
+event: cc.EventListener.MOUSE,
+onMouseDown: function(event){
+ship.engineOn = true;
+},
+onMouseUp: function(event){
+ship.engineOn = false;
+}
+},this)
+
+
+cc.eventManager.addListener({
+event: cc.EventListener.TOUCH_ONE_BY_ONE,
+onTouchBegan: function(event){
+ship.engineOn = true;
+},
+onTouchEnded: function(event){
+ship.engineOn = false;
+}
+},this)
+
+
+
+
+
+
+
+
+background = new ScrollingBG();
+this.addChild(background);
+this.scheduleUpdate();
+this.schedule(this.addAsteroid,0.5);
+ship = new Ship();
+this.addChild(ship);
+},
+update:function(dt){
+background.scroll();
+ship.updateY();
+},
+
+
+addAsteroid:function(event){
+var asteroid = new Asteroid();
+this.addChild(asteroid,1);
+},
+
+removeAsteroid:function(asteroid){
+this.removeChild(asteroid);
+}
+
+});
+var ScrollingBG = cc.Sprite.extend({
+ctor:function() {
+this._super();
+this.initWithFile("assets/background.png");
+},
+onEnter:function() {
+this.setPosition(480,160);
+},
+scroll:function(){
+this.setPosition(this.getPosition().
+x-scrollSpeed,this.getPosition().y);
+if(this.getPosition().x<0){
+this.setPosition(this.getPosition().x+480,this.getPosition().y);
+}
+}
+});
+
+var Ship = cc.Sprite.extend({
+ctor:function() {
+this._super();
+this.initWithFile("assets/ship.png");
+this.scale =0.4;
+this.ySpeed = 0;
+this.engineOn = false;
+},
+onEnter:function() {
+this.setPosition(60,160);
+},
+updateY:function() {
+  if(this.engineOn){
+this.ySpeed += gameThrust;
+}
+this.setPosition(this.getPosition().x,this.getPosition().y+this.
+ySpeed);
+this.ySpeed += gameGravity;
+}
+});
+
+var Asteroid = cc.Sprite.extend({
+ctor:function() {
+this._super();
+this.initWithFile("assets/asteroid.png");
+},
+onEnter:function() {
+this._super();
+this.scale=0.3;
+this.setPosition(600,Math.random()*320);
+var moveAction= cc.MoveTo.create(2.5, new cc.Point(-100,Math.random()*320));
+this.runAction(moveAction);
+this.scheduleUpdate();
+},
+update:function(dt){
+  var shipBoundingBox = ship.getBoundingBox();
+var asteroidBoundingBox = this.getBoundingBox();
+if(cc.rectIntersectsRect(shipBoundingBox,asteroidBoundingBox)){
+gameLayer.removeAsteroid(this);
+cc.audioEngine.playEffect("assets/2.mp3");
+
+
+
+}
+if(this.getPosition().x<-50){
+gameLayer.removeAsteroid(this)
+}
+}
+});
+
+function restartGame(){
+ship.ySpeed = 0;
+ship.setPosition(ship.getPosition().x,160);
+};
---
+++ src/loadassets.js
@@ -0,0 +1,1 @@
+var gameResources = ["assets/background.png", "assets/ship.png", "assets/asteroid.png","assets/1.mp3","assets/2.mp3"  ];
